1bee070425972040ec21cee2cfde2a16ce825dc2
Azie88 <obandoandrew8@gmail.com>
2022-07-05 22:24:00 +0300

    print _putchar with new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
-#include <main.h>
+#include "main.h"
+
 /**
- * main - main block
+ * main - prints _putchar
  * Return: 0
  */
-const char *text="_putchar";
-int main()
+int main(void)
 {
-    int i;
-    for (i=0;text[i]!=0;i++) _putchar(text[i]);
-    _putchar('\n');
-    return (0);
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
 }
